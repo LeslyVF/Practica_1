dc9e0e7b41f5b27b463578df0f6bde8040f933c5
armentaspcs <armentaspcs@DESKTOP-AHUAJBN>
2022-09-06 19:23:47 -0700

    Agregar archivos de proyecto.

---
+++ Practica.1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lesly Vega Fierros, Mexicali B.C. , 21 <3");
+            Console.ReadKey();
+        }
+    }
+}
